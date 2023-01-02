0c6a2768e0fab1c3219841d8ac3fe39da7ccfa18
Ashwin K Whitchurch <ashwin@protocentral.com>
2023-01-02 21:30:06 +0530

    Reformatted

--- software/tinygsr/tinygsr.ino
+++ software/tinygsr/tinygsr.ino
@@ -1,4 +1,4 @@
-// ______          _        _____            _             _ 
+// ______          _        _____            _             _
 // | ___ \        | |      /  __ \          | |           | |
 // | |_/ / __ ___ | |_ ___ | /  \/ ___ _ __ | |_ _ __ __ _| |
 // |  __/ '__/ _ \| __/ _ \| |    / _ \ '_ \| __| '__/ _` | |
@@ -32,77 +32,69 @@
 #include <protocentral_TLA20xx.h>
 
 #define TLA20XX_I2C_ADDR 0x49
-#define CES_CMDIF_PKT_START_1   0x0A
-#define CES_CMDIF_PKT_START_2   0xFA
-#define CES_CMDIF_TYPE_DATA     0x02
-#define CES_CMDIF_PKT_STOP      0x0B
-#define DATA_LEN                9
-#define ZERO                    0
+#define CES_CMDIF_PKT_START_1 0x0A
+#define CES_CMDIF_PKT_START_2 0xFA
+#define CES_CMDIF_TYPE_DATA 0x02
+#define CES_CMDIF_PKT_STOP 0x0B
+#define DATA_LEN 9
+#define ZERO 0
 
 float val;
 int16_t data;
 
 volatile char DataPacket[DATA_LEN];
-const char DataPacketFooter[2] = {ZERO, CES_CMDIF_PKT_STOP};
-const char DataPacketHeader[5] = {CES_CMDIF_PKT_START_1, CES_CMDIF_PKT_START_2, DATA_LEN, ZERO, CES_CMDIF_TYPE_DATA};
+const char DataPacketFooter[2] = { ZERO, CES_CMDIF_PKT_STOP };
+const char DataPacketHeader[5] = { CES_CMDIF_PKT_START_1, CES_CMDIF_PKT_START_2, DATA_LEN, ZERO, CES_CMDIF_TYPE_DATA };
 
 TLA20XX tla2022(TLA20XX_I2C_ADDR);
 
 FIR<float, 8> fir;
 
-void setup() 
-{
-    delay(3000);
-    Serial.begin(57600);
-    Serial.println("Starting ADC...");
+void setup() {
+  delay(3000);
+  Serial.begin(57600);
+  Serial.println("Starting ADC...");
 
-    
-    float coef[8] = { 1., 1., 1., 1., 1., 1., 1., 1.};
-     // Set the coefficients
-    fir.setFilterCoeffs(coef);
+  float coef[8] = { 1., 1., 1., 1., 1., 1., 1., 1. };
+  // Set the coefficients
+  fir.setFilterCoeffs(coef);
 
-    //Wire.setSDA(4);
-    //Wire.setSCL(5);
+  Wire.begin();
 
-    Wire.begin();
+  tla2022.begin();
 
-    tla2022.begin();
-    
-    tla2022.setMode(TLA20XX::OP_CONTINUOUS);
-    tla2022.setDR(TLA20XX::DR_128SPS);
-    tla2022.setFSR(TLA20XX::FSR_0_512V);
+  tla2022.setMode(TLA20XX::OP_CONTINUOUS);
+  tla2022.setDR(TLA20XX::DR_128SPS);
+  tla2022.setFSR(TLA20XX::FSR_0_512V);
 }
 
-void loop() 
-{
-    val = fir.processReading(tla2022.read_adc()); // +/- 2.048 V FSR, 1 LSB = 1 mV
-    data = (int16_t)val;
+void loop() {
+  val = fir.processReading(tla2022.read_adc());  // +/- 2.048 V FSR, 1 LSB = 1 mV
+  data = (int16_t)val;
 
-    //Serial.println(data);
-    
-    DataPacket[0] = (uint8_t)data;
-    DataPacket[1] = (uint8_t)(data >> 8);
-    DataPacket[2] = 0x00;
-    DataPacket[3] = 0x00;
+  //Serial.println(data);
+
+  DataPacket[0] = (uint8_t)data;
+  DataPacket[1] = (uint8_t)(data >> 8);
+  DataPacket[2] = 0x00;
+  DataPacket[3] = 0x00;
 
   //send packet header
-  for(int i=0; i<5; i++){
+  for (int i = 0; i < 5; i++) {
 
     Serial.write(DataPacketHeader[i]);
   }
 
   //send actual data
-  for(int i=0; i<DATA_LEN; i++){
+  for (int i = 0; i < DATA_LEN; i++) {
 
     Serial.write(DataPacket[i]);
   }
 
   //send packet footer
-  for(int i=0; i<2; i++){
+  for (int i = 0; i < 2; i++) {
 
-   Serial.write(DataPacketFooter[i]);
+    Serial.write(DataPacketFooter[i]);
   }
-
-    
-    delay(8);
+  delay(8);
 }
